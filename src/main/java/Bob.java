fb0731a9cda35245c9def9c7747f45e4ece70221
Aaron Contreras <aaronlcaq@gmail.com>
2020-03-02 17:14:27 -0500

    Refactor conditional tests into explanatory methods

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,31 +1,49 @@
 public class Bob {
     
-    private static final String PERIOD = ".";
-    private static final String EXCLAMATION = "!";
     private static final String QUESTION_MARK = "?";
 
     public String hey(String incoming) {
+
         incoming = incoming.trim();
 
         if (incoming.isEmpty())
             return "Fine. Be that way!";
 
-        if (incoming.equals(incoming.toLowerCase()) && !incoming.endsWith(QUESTION_MARK))
+        if (isNumerical(incoming))
             return "Whatever.";        
 
-        if (incoming.equals(incoming.toLowerCase()) && incoming.endsWith(QUESTION_MARK))
+        if (isNumericalQuestion(incoming))
             return "Sure.";
 
-        if (incoming.equals(incoming.toUpperCase()) && incoming.endsWith(QUESTION_MARK))
+        if (isYellingQuestion(incoming))
             return "Calm down, I know what I'm doing!";
 
-        if (incoming.equals(incoming.toUpperCase()))
+        if (isYelling(incoming))
             return "Whoa, chill out!";
 
-        if (incoming.endsWith(QUESTION_MARK))
+        if (isQuestion(incoming))
             return "Sure.";
 
         return "Whatever.";
+    }
+
+    public boolean isNumerical(String incoming) {
+        return incoming.equals(incoming.toLowerCase()) && !incoming.endsWith(QUESTION_MARK);
+    }
+
+    public boolean isNumericalQuestion(String incoming) {
+        return incoming.equals(incoming.toLowerCase()) && incoming.endsWith(QUESTION_MARK);
+    }
+
+    public boolean isYellingQuestion(String incoming) {
+        return incoming.equals(incoming.toUpperCase()) && incoming.endsWith(QUESTION_MARK);
+    }
+
+    public boolean isYelling(String incoming) {
+        return incoming.equals(incoming.toUpperCase());
+    }
+    public boolean isQuestion(String incoming) {
+        return incoming.endsWith(QUESTION_MARK);
     }
 }
 
