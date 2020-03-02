483523fa14085a2eb043baedb6cba92180f3c6da
Aaron Contreras <aaronlcaq@gmail.com>
2020-03-02 16:43:58 -0500

    First solution

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,10 +1,46 @@
-/*
+public class Bob {
+    
+    private static final String PERIOD = ".";
+    private static final String EXCLAMATION = "!";
+    private static final String QUESTION_MARK = "?";
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    public String hey(String incoming) {
+        incoming = incoming.trim();
 
-Please remove this comment when submitting your solution.
+        if (incoming.isEmpty())
+            return "Fine. Be that way!";
 
-*/
+        if (incoming.equals(incoming.toLowerCase()) && !incoming.endsWith(QUESTION_MARK))
+            return "Whatever.";        
+
+        if (incoming.equals(incoming.toLowerCase()) && incoming.endsWith(QUESTION_MARK))
+            return "Sure.";
+
+        if (incoming.equals(incoming.toUpperCase()) && incoming.endsWith(QUESTION_MARK))
+            return "Calm down, I know what I'm doing!";
+
+        if (incoming.equals(incoming.toUpperCase()))
+            return "Whoa, chill out!";
+
+        if (incoming.endsWith(QUESTION_MARK))
+            return "Sure.";
+
+        return "Whatever.";
+    }
+}
+
+
+
+/*Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+
+Bob answers 'Sure.' if you ask him a question.
+
+He answers 'Whoa, chill out!' if you yell at him.
+
+He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
+
+He says 'Fine. Be that way!' if you address him without actually saying anything.
+
+He answers 'Whatever.' to anything else.
+
+Bob's conversational partner is a purist when it comes to written communication and always follows normal rules regarding sentence punctuation in English. */
